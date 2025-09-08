4b4605f5feff32a437a4dbbd3e561764b259aeb7
Felix Riddle <fedecraft3312@gmail.com>
2025-09-08 12:33:20 -0300

    Cars

---
+++ src/carsExercise.py
@@ -0,0 +1,10 @@
+# Cars and colors
+redCars = 56;
+whiteCars = 168;
+blackCars = 227;
+grayCars = 49;
+otherColorCars = 37;
+totalCars = redCars + whiteCars + blackCars + grayCars + otherColorCars;
+
+print("Total cars: ", totalCars);
+
